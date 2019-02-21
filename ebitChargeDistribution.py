e97c8ceeb2e8921a0ed1b93d2c9ce207eef7b871
Jon Ringuette <bobskr@gmail.com>
2019-02-21 09:34:56 -0700

    progress is happening, too much to write up

--- ebitChargeDistribution.py
+++ ebitChargeDistribution.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 from math import *
+from IonizationEnergies import *
+from rr import *
+
 
 __EMASS__ = 5.11e5  # "Electron mass in eV"
 __C__ = 3.0e10  # "Speed of light"
@@ -16,12 +19,6 @@ __MAXSPECIES__ = 1000
 
 
 class Species:
-    Z = 0
-    A = 0
-    decaysTo = 0.0
-    betaHalfLife = 0.0
-    initSCIPop = 1.0
-
     def __init__(self, Z, A, decaysTo=0.0, betaHalfLife=0.0, initSCIPop=1.0):
         self.Z = Z
         self.A = A
@@ -30,19 +27,108 @@ class Species:
         self.initSCIPop = initSCIPop
 
 
+class RkStepParams:
+    def __init__(self, minCharge=5e-5, tStep=1e-6, desiredAccuracyPerChargeState=1e-4):
+        self.minCharge = minCharge
+        self.tStep = tStep
+        self.desiredAccuracyPerChargeState = desiredAccuracyPerChargeState
+
+
 def createEmptyListofLists(species):
     emptylist = [[0.0 for i in range(species[0].Z + 2)] for j in range(len(species) + 1)]  # Init multidimentional array w/ 0's
     return emptylist
 
 
 def createDefaultPopulation(species):
-    species.sort(key=lambda x: x.Z, reverse=True)  # Sort by Z in decending order
 # length of new array needs to be 1+# of species and 2+maxZ value
     populationList = createEmptyListofLists(species)
     for i in range(0, len(species)):
         populationList[i][1] = species[i].initSCIPop
 
     return populationList
+
+
+def createDecayConstants(species):
+    decayConstants = [0] * (len(species) + 1)
+
+    if species[0].betaHalfLife != 0.0:
+        raise ValueError("Can not handle having a beta decay for highest Z species")
+
+    for i in range(0, len(species)):
+        if species[i].betaHalfLife <= 0:
+            decayConstants[i] = 0.0
+        else:
+            decayConstants[i] = log(2) / species[i].betaHalfLife
+    return decayConstants
+
+def createChargeExchangeRates(Z, A, pressure, ionTemperature):
+    chargeExchangeRates = [0] * (Z + 1)
+
+    # Need to return a Z+1 array
+    h2Density = pressure * __TORR__
+    ionMassInAMU = A * __AMU__
+    avgIonV = __C__ * sqrt(8.0 * (ionTemperature / (pi * ionMassInAMU)))
+    avgIonVinBohr = avgIonV / __VBOHR__
+    sigV = __CHEXCONST__ * log( 15.0 / avgIonVinBohr) * avgIonV
+
+
+    for i in range(1, Z + 1):
+        chargeExchangeRates[i] = i * sigV * h2Density
+    return chargeExchangeRates
+
+
+def createInteractionRates(Z, beamEnergy, currentDensity, crossSections):
+    # From Lisp Code :
+    #  "Calculate the rate for a specific interaction (ionization/rr/..)
+    #  inside the trap given the BEAM-ENERGY in eV, CURRENT-DENSITY in
+    #  A/cm2, and the CROSS-SECTIONS in cm-2 corresponding to the
+    #  interaction. Returns array of size Z-ION+1 with rates."
+    interactionRate = [0] * (Z + 1)
+
+    electronVelocity = __C__ * sqrt(2 * (beamEnergy / __EMASS__))
+    electronRate = currentDensity / __ECHG__ / electronVelocity
+
+    for i in range(0, Z + 1):
+        interactionRate[i] = crossSections[i] * electronVelocity * electronRate
+
+    return interactionRate
+
+
+def createDefaultInteractionRates(species, beamEnergy, currentDensity, myfunc, pressure=0, ionTemperature=0, chex=0):
+    interactionRates = createEmptyListofLists(species)
+    for i in range(0, len(species)):
+
+        if chex == 0:
+            myFuncValues = createInteractionRates(species[i].Z, beamEnergy, currentDensity, myfunc(beamEnergy, species[i].Z))
+        else:
+            myFuncValues = myfunc(species[i].Z, species[i].A, pressure, ionTemperature)
+
+        for r in range(0, len(myFuncValues)):
+            interactionRates[i][r] = myFuncValues[r]
+
+    return interactionRates
+
+
+def calculateK():
+    return
+
+
+def rkStep():
+    return
+
+
+def adaptiveRkStepper(breedingTime, probeEvery, probeFn, population, species, decayConstants, ionizationRates, chexRates, rrRates, rkParams):
+    #  Must create some arrays...
+    k1 = k2 = k3 = k4 = tmp = y1 = y12 = y22 = createEmptyListofLists(species)
+    time = 0.0
+    nextPrint = 0.0
+    noTooBigSteps = 0
+    step = rkParams.tStep  # Not sure if there is method to this madness yet..
+    bestStepSize = step  # Currently just translating directly will try to be more nuanced after another pass
+
+    desiredAccuracy = rkParams.desiredAccuracyPerChargeState / species[0].Z
+
+    return
 
 
 def calcChargePopulations(species,
@@ -57,7 +143,7 @@ def calcChargePopulations(species,
                           ionTemperature=100.0,
                           currentDensity=None,
                           population=None,
-                          decayConsants=None,
+                          decayConstants=None,
                           ionizationRates=None,
                           rrRates=None,
                           chexRates=None,
@@ -65,12 +151,28 @@ def calcChargePopulations(species,
 
     if currentDensity is None:
         currentDensity = (ionEbeamOverlap * beamCurrent) / (pi * (beamRadius ** 2))
-    mine = []
 
-    mine.append(Species(4, 107, 0.0, 0.0, 7.0))
-    mine.append(Species(2, 107, 0.0, 0.0, 6.0))
-    mine.append(Species(6, 107, 0.0, 0.0, 5.0))
-    createDefaultPopulation(mine)
+    species = []
+    species.append(Species(4, 107, 0.0, 0.0, 7.0))
+    species.append(Species(2, 107, 0.0, 1.0, 6.0))
+    species.append(Species(6, 107, 0.0, 0.0, 5.0))
+    species.sort(key=lambda x: x.Z, reverse=True)  # Sort by Z in decending order
 
-#    if population is None:
-#        population = createDefaultPopulation(species)
+    if population is None:
+        population = createDefaultPopulation(species)
+
+    if decayConstants is None:
+        decayConstants = createDecayConstants(species)
+
+    if ionizationRates is None:
+        ionizationRates = createDefaultInteractionRates(species, beamEnergy, currentDensity, createIonizationCrossSections)
+
+    if rrRates is None:
+        rrRates = createDefaultInteractionRates(species, beamEnergy, currentDensity, createRRCrossSections)
+
+    if chexRates is None:
+        chexRates = createDefaultInteractionRates(species, beamEnergy, currentDensity, createChargeExchangeRates, pressure, ionTemperature, 1)
+
+    rkParams = RkStepParams()
+
+    adaptiveRkStepper(breedingTime, probeEvery, probeFn, population, species, decayConstants, ionizationRates, chexRates, rrRates, rkParams)
---
+++ rr.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+
+from math import *
+# These are set to the values they are because that is how the lisp
+# code I translated this from did it...
+__GSIZE__ = 101
+__NMAX__ = 30
+
+
+def g0():
+    # I have little to no idea what this does nor did the person
+    # who wrote the original lisp code I translated this from...
+    # it's definitly a matrix though!
+    # If you know what this does please let me know and I will owe
+    # you a beer.
+    global __GSIZE__
+    global __NMAX__
+    g0_list = [0.0] * __GSIZE__  # init list to all 0.0's
+    g0_list[0] = sqrt(8 * pi) * exp(log(4) - 2)
+
+    for n in range(1, __NMAX__):
+        g0_list[n] = (g0_list[n-1] * 4 * n * exp((n + 1) * log((n + 1) / n))) \
+            / sqrt((2 * (n + 1) - 1)) \
+            / sqrt(2 * (n + 1) - 2) \
+            / exp(2)
+    return g0_list
+
+
+def determineShell(q, z):
+
+    nmin = 0
+    lstart = 0
+    num_electrons = z - q
+
+    if q == z:
+        nmin = 1
+    else:
+        while num_electrons > 0:
+            num_electrons = num_electrons - 2
+            nmin = nmin + 1
+
+            if num_electrons < 0:
+                break
+            for l in range(1, nmin):
+                num_electrons = num_electrons - ((4 * l) + 2)
+                if num_electrons < 0:
+                    lstart = l
+                    break
+    if num_electrons != 0:
+        lastval = abs(num_electrons) / (((lstart * 2) + 1) * 2)
+    else:
+        lastval = 1
+    return nmin, lstart, lastval
+
+
+def createGGArray():
+    # Create multidimention list that is 101 x 101... yup..
+    gg = [[0] * __GSIZE__ for i in range(__GSIZE__)]
+    return gg
+
+
+def createGG(n, kappa, g0, gg):
+    # Starts populating the gg matrix with stuff?
+    f1 = 1 + (n * kappa) ** 2
+    f = ((sqrt(1 / (1 - exp(-(2.0 * pi) / kappa))) * exp((2.0 * n) - (2.0 / kappa) * atan(n * kappa)) / f1 ** 2) * g0[n - 1])
+
+    for s in range(1, n + 1):
+        f = (f * sqrt(1+ (s * kappa) ** 2)) / f1
+    gg[n - 1][n] = f
+
+    if (n - 2) < 0:
+        return gg
+
+    gg[n - 2][n - 1] = 0.5 * sqrt(((2.0 * n) - 1) * f1) * f
+    gg[n - 1][n - 2] = (0.5 / n) * sqrt(f1 / (1 + ((n - 1) * kappa) ** 2)) * f
+
+    if (n - 3) < 0:
+        return gg
+    gg[n - 2][n - 3] = ((((n - 1) * f1) + 4) / 2 / n) * sqrt(((2 * n) - 1) / (1 + ((n - 2) * kappa) ** 2)) * gg[n - 1][n - 2]
+    for l in range(n - 1, 1, -1):
+        a = (((n ** 2) - (l ** 2)) * 4) + (((2 * l) - 1) * l * f1)
+        b = -2 * n * sqrt((1 + ((1 + l) * kappa) ** 2) * ((n ** 2) - (l ** 2)))
+        c = 2 * n * sqrt((1 + ((l * kappa) ** 2)) * ((n ** 2) - ((l - 1) ** 2)))
+        d = ((4 * (n ** 2) ) - (4 * (l ** 2))) + (l * (1 + (2 * l)) * f1)
+        e = -2 * n * sqrt(((n ** 2) - ((l + 1) ** 2)) * (1 + ((l * kappa) ** 2)))
+        f = 2 * n * sqrt(((n ** 2) - (l ** 2)) * (1 + (((l - 1) * kappa) ** 2)))
+
+        gg[l - 2][l - 1] = ((a * gg[l - 1][l]) + (b * gg[l][l + 1])) / c
+
+        if l < (n - 1):
+            gg[l - 1][l - 2] = ((d * gg[l][l - 1]) + (e * gg[l + 1][l])) / f
+
+    return gg
+
+def rrCrossSection(eKinetic, q, Z, gg=createGGArray(), g0=g0()):
+    ry = 13.605698
+    alpha = 1 / 137.036
+    a0 = 5.29177249e-9
+    const = (4 * pi * alpha * (a0 ** 2)) / 3
+    k = sqrt(eKinetic / ry)
+    eta = (q + Z) / (2 * k)
+    cross = 0.0
+
+    nmin, lstart, factor = determineShell(q, Z)
+
+    for n in range(nmin, __NMAX__ + 1):
+        crossShell = 0
+        if n == nmin:
+            loop = lstart
+        else:
+            loop = 0
+        for l in range(loop, n):
+            if q == Z:
+                zeff = Z
+            else:
+                zeff = (((q + Z) * 0.5) - (((Z - q) * 0.5 * (eta - 1)) / (eta + 1 + (3 * l)))) * exp(-(0.05 * (l - 1) ** 2))
+
+            kappa = k / zeff
+            gg = createGG(n, kappa, g0, gg)  # **Still need to figure out if this should be set to gg or a new var
+            kk = (k * k) + ((zeff ** 2) / (n ** 2))
+
+            tmpCross = (l + 1) * (gg[l][l + 1] ** 2) * ((1 + (n ** 2) * (kappa ** 2)))
+            if l > 0:
+                tmpCross = tmpCross +  ((l) * (gg[l][l - 1] ** 2) * ((1 + (n ** 2) * (kappa ** 2))))
+            tmpCross = (tmpCross * n * n * const) / zeff / zeff
+            crossSubShell = 0.5 * tmpCross * (((alpha * kk) / k) ** 2)
+            if (n == nmin) and (l == lstart):
+                crossSubShell = crossSubShell * factor
+
+            if (l > loop) and (crossSubShell < 1e-50):
+                break
+            else:
+                crossShell = crossShell + crossSubShell
+            cross = cross + crossSubShell
+    return(cross)
+
+def createRRCrossSections(ElectronEnergy, Zion):
+    crossSections = [0] * (Zion + 1)
+    for q in range(1, Zion + 1):
+        crossSections[q] = rrCrossSection(ElectronEnergy, q, Zion)
+
+    return(crossSections)
